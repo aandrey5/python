549b78b8ff22b80da11d4d0d360608d0f9a881e4
aandrey5 <aandrey5@gmail.com>
2020-09-23 23:15:18 +0300

    Доработка кодировки в json структуре

--- lesson_5_7.py
+++ lesson_5_7.py
@@ -42,8 +42,8 @@ with open('text_8.txt', 'r', encoding='utf-8') as main:
     print(output_list)
     main.seek(0)
 
-with open('output.json', 'w') as write_json:
-    json.dump(output_list, write_json)
+with open('output.json', 'w', encoding='utf-8') as write_json:
+    json.dump(output_list, write_json, indent=4, ensure_ascii=False)
 dvd()
 print('Создали файл output.json')
 dvd()
@@ -52,7 +52,7 @@ dvd()
 print('Считываем обратно из output.json')
 print()
 
-with open('output.json', 'r') as read_json:
+with open('output.json', 'r', encoding='utf-8') as read_json:
     data = json.load(read_json)
 print(data)
 
