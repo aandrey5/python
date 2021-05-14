0a46abcfbdc66efa743cf0441449c82ace416adb
aandrey5 <60394118+aandrey5@users.noreply.github.com>
2021-05-14 19:45:34 +0300

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,6 +1,10 @@
 # Replace . to , in df pandas column - IN PLACE
 df2['sum_purchase_nds'].str.replace(',' , '.')
 
+# OR
+
+df[list_cols] = df[list_cols].apply(lambda s: s.str.replace(',' , '.')
+
 # View types columns
 
 df1.dtypes
